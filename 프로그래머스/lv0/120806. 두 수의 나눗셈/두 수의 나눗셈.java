67bb1b31b73c7d970b2377152ced93233138f2c4
seran <120924270+daisyy0000@users.noreply.github.com>
2023-04-27 17:19:37 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -2,13 +2,10 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
         if(num1 >0 && num1 <= 100 && num2 > 0 && num2 <= 100){
-            float a1 = (float)num1;
-            float a2 = (float)num2;
-            float test = a1 / a2;
-    
-            answer = (int)(test * 1000);
+            double result = (double)num1 / (double) num2;
+            answer = (int)(result * 1000);
             return answer;
         }
-        return answer;
+        return -1;
     }
 }
