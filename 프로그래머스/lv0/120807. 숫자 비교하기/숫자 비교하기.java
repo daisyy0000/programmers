5e12626ccf32b56345f14b1eb91782e2b55feadf
seran <120924270+daisyy0000@users.noreply.github.com>
2023-04-28 23:52:27 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -5,5 +5,9 @@ class Solution {
             return 1;
         }
         return -1;
+        
+        //다른분의 풀이 (삼항연산자 (boolean자료형으로 참 또는 거짓) ? 참일 때 연산결과 : 거짓일 때 연산결과)
+        //int answer = (num1 == num2) ? 1 : -1;
+        //return answer;
     }
 }
