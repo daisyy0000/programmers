9036945a689f6741c6a5464a0069298a1b50d7d9
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-04 10:12:09 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 73.7 MB -BaekjoonHub (보완)

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -2,6 +2,37 @@ class Solution {
     public int[] solution(int n) {
         int[] answer = {};
         
+        //(보완) 방법1: 처음부터 배열의 크기를 지정, 안에들어가는건 똑같이 홀수니깐(똑같은 코드 발생)
+      
+        /*
+        int[] answer;
+        if (n % 2 == 0) {
+            answer = new int[n / 2];
+        } else {
+            answer = new int[(n + 1) / 2];
+        }
+        
+        for (int i = 0; i < answer.length; i++) {
+                answer[i] = (i * 2) + 1;
+            }
+        return answer;
+        */
+        
+        
+        //(보완) 방법2: ArrayList로
+        
+        /*
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+
+        for(int i=1; i<=n; i++){
+          if(i%2 != 0) {
+              answer.add(i);
+          } 
+        }
+        return answer;         //(그냥참고만..) 배열로 자료구조 바꾸는 코드: answer.stream().mapToInt(x -> x).toArray();
+       */
+        
+        
         //n이 짝수일 경우
         if(n % 2 == 0){
             answer = new int[n/2];
