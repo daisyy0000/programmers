ee9ac70623c9001e73cb1d4be7d25511dec55d96
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-05 17:54:20 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 70.1 MB -Baekjoonhub (보완)

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -2,6 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
+        //(보완) 방법1: 삼항연산자
+        //int answer = (n%7==0) ? n/7 : n/7 + 1;
+       
         if(n % 7 == 0){
             answer = n / 7; 
         }else if(n % 7 != 0){
