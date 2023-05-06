8cc50c319d56f9e7f7d647829b6cf357508f9da2
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-06 13:42:38 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.63 ms, Memory: 73.4 MB -Baekjoonhub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -2,6 +2,21 @@ import java.math.*;
 
 class Solution {
     public int solution(int n) {
+        
+        // 다른사람들 풀이..(나는 언제쯤 간단하게 짤 수 있을까..)
+        /*
+        int answer = 1;
+
+       while(true){
+            if(6 * answer % n == 0){
+                break;
+            }
+            answer++;
+        }
+        return answer;
+        */
+            
+        
         int answer = 0;
         int pizza = 6;
         
