8f89cb5e0c799c26c1dd405e89b7f06b5caaf225
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-09 09:56:26 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,19 @@
+import java.lang.*; 
+
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+       
+        if(100000 <= price && price < 300000){
+           answer =  (int)(price*0.95);
+        }else if(300000 <= price && price < 500000){
+            answer = (int)(price*0.9);
+        }else if(500000 <= price && price <=1000000){
+            answer = (int)(price*0.8);
+        }else{
+            answer = price;
+        }
+        
+        return answer;
+    }
+}
