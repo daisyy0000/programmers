45260c4e2ecfa00e57540846e23e7d6f0ef77620
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-10 15:30:59 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,14 +1,29 @@
 class Solution {
     public int[] solution(int money) {
-        int coffee = 0;
+        //커피잔 수
+        int cup = 0;
+        
+        //아메리카노(아메리카노 가격이 수시로 바뀐다는 가정에..바로 숫자로 적으면..나중에 수정할떄..복잡해지니깐!일단 변수로 담아두자..)
+        int coffee = 5500;
+        
+        //잔돈
         int change = money;
         
-        if(money >= 5500){
-        coffee = money / 5500;
-        change = money - 5500 * coffee;
+        if(money >= coffee){
+        cup = money / coffee;
+        change = money - coffee * cup;
         }
        
-        int[] answer = {coffee, change};
+        int[] answer = {cup, change};
         return answer;
+        
+        //다른 사람들의 풀이
+        /*
+        int[] answer = new int[2];
+        answer[0] = money/5500;
+        answer[1] = money%5500;
+        return answer;
+
+        */
     }
 }
