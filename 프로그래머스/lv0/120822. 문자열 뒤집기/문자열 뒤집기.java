9bfc8e22a6c6667f826efbd7c8c50616e91739fc
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-13 09:50:32 +0900

    [level 0] Title: 문자열 뒤집기, Time: 9.12 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,10 +2,12 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        
         for(int i = my_string.length()-1; i >= 0; i--){
             answer = answer + my_string.charAt(i);
         }
         return answer;
+        
+        //다른사람들 풀이(StringBuilder이용해서..! ㅜ한줄로 딱!!)
+        //return new StringBuilder(myString).reverse().toString();
     }
 }
