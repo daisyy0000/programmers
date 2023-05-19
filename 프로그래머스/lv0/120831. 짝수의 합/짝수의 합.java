0cf76b25777125d47df7a5515c91e925b708b696
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-20 00:21:50 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -7,7 +7,13 @@ class Solution {
            answer += (2*i); 
         }
         
-
+        //다른사람 풀이
+        /*
+        for (int i = 0; i <= n; i++){
+            if (i % 2 == 0){
+                answer += i;
+        */
+        
         return answer;
     }
 }
