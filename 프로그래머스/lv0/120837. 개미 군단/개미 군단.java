4547a513b1f19e08270ee695d51eae28df4569a8
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-25 23:58:30 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        int jangGun = 5;
+        int byeongjeong = 3;
+        int il = 1;
+         
+        int quotient1 = hp / jangGun;
+        int remainder1 = hp % jangGun;
+        
+        int quotient2 = 0;
+        int remainder2 = 0;
+ 
+        if(remainder1 != 0 || quotient1 == 0){
+            quotient2 = remainder1 / byeongjeong; 
+            remainder2 = remainder1 % byeongjeong;
+        }
+            
+        answer = quotient1 + quotient2 + remainder2;
+        return answer;
+    }
+}
