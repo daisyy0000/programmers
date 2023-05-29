e9c5116a25d0cfaf5b5224e2b15324e68e0d46fb
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-29 14:37:24 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[][] solution(int[] num_list, int n) {
         int[][] answer = new int[num_list.length/n][n];
+        //int cnt = 0;
         
         for(int i=0; i < num_list.length/n; i++){
             for(int j=0; j < n; j++){
@@ -9,6 +10,12 @@ class Solution {
                 }else if(j%n != 0){
                    answer[i][j] = num_list[(i*n)+j];
                 }
+                
+                //다른 풀이는 for문안에 나처럼 if문으로 안하고..바로
+                /*
+                answer[i][j] = num_list[cnt];
+                cnt++;
+                */
             }
         }
         return answer;
