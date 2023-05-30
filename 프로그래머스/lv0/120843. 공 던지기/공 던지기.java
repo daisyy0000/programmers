bbd335275afc02cd18835f445ed763fadac9172d
seran <120924270+daisyy0000@users.noreply.github.com>
2023-05-30 22:33:49 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -2,8 +2,11 @@ class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 0;
         
+        //이걸 반복을 돌릴필요가없는디..왜for문으로 했지..
         for(int i=0; i<2*k; i+=2){
-           answer = numbers[i%numbers.length];
+           answer = numbers[i%numbers.length]; \
+           //다른 사람풀이..한줄...numbers[((k-1)*2)%numbers.length]
+           //내가 쓴 i가 (k-1)*2..!
         }
         return answer;
     }
